6bf62f2c7c410ed69aa0053c5c17e88662928401
Christy Lum <christy.lum121@gmail.com>
2020-06-27 21:47:31 +0000

    Added even.js file

---
+++ even.js
@@ -0,0 +1,14 @@
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+/* 
+Can be omitted to simplify code
+We don't need to set our function return values to new variables before doing something with them
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+*/
+
+console.log(isEven(10));
+console.log(isEven(11));
