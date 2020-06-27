ec9379e3f5366b423bb364ad32740f409498abe0
Christy Lum <christy.lum121@gmail.com>
2020-06-27 22:00:11 +0000

    Added odd.js file, which determines whether a number is odd or not.

---
+++ odd.js
@@ -0,0 +1,8 @@
+//Create a function to determine if a number is odd or not.
+
+const isOdd = function(num) {
+  return num % 2 === 1;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
