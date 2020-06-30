3b7d679d5f26f4a2d9dafa040990d7c81b5fe68a
Christy Lum <christy.lum121@gmail.com>
2020-06-30 23:05:15 +0000

    Completed code for concatenating arrays assignment

---
+++ concat-arrays.js
@@ -0,0 +1,8 @@
+function concat (arr1, arr2) {
+  for (var i = 0; i < arr2.length; i++) {
+    arr1.push(arr2[i]);
+  }
+  return arr1;
+}
+//console.log(concat([1,2,3],[4,5,6]), "=?", [1, 2, 3, 4, 5, 6]);
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
